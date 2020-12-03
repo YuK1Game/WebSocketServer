b4c908006387c37b3e357f22a9677dc9e7ff32c2
YuK1Game <yuk1.wcat@gmail.com>
2020-12-03 15:25:45 +0900

    feat: 途中

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/public-event', function(){
+    broadcast(new \App\Events\PublicEvent);
+    return 'public';
+});
